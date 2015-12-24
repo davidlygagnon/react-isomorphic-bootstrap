4b550238e4768a71d32b7b4c009c49f8a7e3cbb6
davidlygagnon <david.lygagnon@gmail.com>
2015-12-23 23:04:59 -0800

    test

--- deploy.js
+++ deploy.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'production') {
   // We basically just create a child process that will run
   // the production bundle command
   var child_process = require('child_process');
-  child_process.exec("webpack -p --config webpack.config.js", function (error, stdout, stderr) {
+  child_process.exec("./node_modules/.bin/webpack -p --config webpack.config.js", function (error, stdout, stderr) {
     console.log('RAN deploy.jsstdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
